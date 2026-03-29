8602f897ee2b12173dd300b284b292e414726e3c
Aubrey Leavenworth <aule6714@colorado.edu>
2026-03-29 12:49:11 -0600

    removed composite key from multi-relation table

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -22,6 +22,5 @@ CREATE TABLE IF NOT EXISTS tags (
 );
 CREATE TABLE IF NOT EXISTS note_to_tag (
 note_id INT NOT NULL,
-tag_id INT NOT NULL,
-note_to_tag_key PRIMARY KEY (note_id,tag_id)--composite key
+tag_id INT NOT NULL
 );
