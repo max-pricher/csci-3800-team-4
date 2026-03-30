0f1e2577de257fa012a5059fb09469996de73d69
Aubrey Leavenworth <aule6714@colorado.edu>
2026-03-30 00:25:31 -0600

    Editing database variable names and fixing syntax

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,24 +1,22 @@
 CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
+    user_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     pwd VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS notes (
-    id SERIAL PRIMARY KEY,
-    user_id INT,
+    note_id SERIAL PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     body VARCHAR(200) NOT NULL,
-    time_made DATETIME NOT NULL DEFAULT(GETDATE()),
-    CONSTRAINT user_id FOREIGN KEY (user_id) REFERENCES users(id)
+    time_made TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_user_id FOREIGN KEY (id) REFERENCES users(id)
 
 );
 CREATE TABLE IF NOT EXISTS tags (
-    id SERIAL PRIMARY KEY,
-    user_id INT,
+    tag_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     color VARCHAR(7) NOT NULL --should be a hex color input like #33ddff
-    CONSTRAINT user_id FOREIGN KEY (user_id) REFERENCES users(id)
+    CONSTRAINT fk_user_id FOREIGN KEY (id) REFERENCES users(id)
 );
 CREATE TABLE IF NOT EXISTS note_to_tag (
 note_id INT NOT NULL,
