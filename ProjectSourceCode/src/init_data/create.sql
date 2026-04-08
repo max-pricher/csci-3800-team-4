243bf84d2ed43c94ea3b1bdc8e4f53976e98e93c
Max Pricher term <mapr3537@colorado.edu>
2026-04-07 22:43:32 -0600

    added Lorem Ipsum test note
    added a longer standard note test

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -62,4 +62,5 @@ INSERT INTO users (username, password) VALUES --default user
 
 INSERT INTO notes (user_id, title, body) VALUES
 (1, 'Test Note 1', 'This is the body of our first test note 1.'),
-(1, 'Test Note 2', 'This is the body of our second test note, but im gonna make this one just a little bit longer, if thats ok with you. Ok. what should i talk about. Do you guys like dogs?.');
+(1, 'Test Note 2', 'This is the body of our second test note, but im gonna make this one just a little bit longer, if thats ok with you. Ok. what should i talk about. Do you guys like dogs?.'),
+(1, 'Lorem Ipsum', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
