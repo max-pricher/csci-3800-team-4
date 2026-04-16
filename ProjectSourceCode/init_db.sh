2f7559ad6fd9d1a03ec0dff7fd27ffcfeb9fe0d5
Max Pricher term <mapr3537@colorado.edu>
2026-04-16 17:46:23 -0600

    omg

--- ProjectSourceCode/init_db.sh
+++ ProjectSourceCode/init_db.sh
@@ -2,9 +2,9 @@
 # DO NOT PUSH THIS FILE TO GITHUB
 # This file contains sensitive information and should be kept private
 # TODO: Set your PostgreSQL URI - Use the External Database URL from the Render dashboard
-PG_URI="postgresql://loconotion_db_user:8ch33StU86sO9YCP4Mz3Zqt4F13iW3dw@dpg-d7glgu9kh4rs739buuq0-a.oregon-postgres.render.com/loconotion_db"
+PG_URI="postgresql://exampleuser:0QgLdbjYNocXAyWVG3Mq5LSI1g2EcZPa@dpg-d7gms7hj2pic738p4lvg-a.oregon-postgres.render.com/loconotion_db_ers1"
 # Execute each .sql file in the directory
-for file in init_data/*.sql; do
+for file in src/init_data/*.sql; do
     echo "Executing $file..."
     psql $PG_URI -f "$file"
 done
